93b10430e109a78a46b6b4512617074453cacc84
Gabriel Ortiz <Gabriel Ortiz>
2017-03-05 19:03:27 -0800

    Map Author Data for Dropdown work

--- BuildingApplicationsWithReactAndReduxInES6/src/components/course/ManageCoursePage.js
+++ BuildingApplicationsWithReactAndReduxInES6/src/components/course/ManageCoursePage.js
@@ -2,32 +2,55 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import * as authorActions from '../../actions/authorActions';
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends React.Component {
     constructor(props, context) {
         super(props, context);
+
+        this.state = {
+            course: Object.assign({}, props.course),
+            authors: props.authors,
+            errors: {}
+        };
     }
 
     render() {
         return (
-            <h1>Manage Course</h1>
+            <CourseForm 
+                allAuthors={this.props.authors}
+                course={this.state.course}
+                errors={this.state.errors}
+            />
         );
     }
 }
 
 ManageCoursePage.propTypes = {
-    //myProp: PropTypes.string.isRequired
+    course: PropTypes.object.isRequired,
+    authors: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
+    let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+
+    const authorsFormattedForDropdown = state.authors.map(author => {
+        return {
+            value: author.id,
+            text: author.firstName + ' ' + author.lastName
+        };
+    });
+
     return {
-        state: state
+        course: course,
+        authors: authorsFormattedForDropdown
     };
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        actions: bindActionCreators(courseActions, dispatch)
+        actions: bindActionCreators(courseActions, authorActions, dispatch)
     };
 }
 

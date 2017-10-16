f5c951d30a918b2ebdc6aea79fb847579a67bfd8
onlyonegabe <gabriel.j.ortiz@gmail.com>
2017-10-15 21:22:39 -0700

    Initial ReactReduxWorkshop work Modules 1-4

---
+++ ReactReduxWorkshop/dev/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import LoginForm from "./components/app/loginForm.jsx";
+
+require("./site.scss");
+
+ReactDOM.render(
+    <LoginForm />,
+    document.querySelector("#container")
+)

7c29a3d4cb18cda6e7e97f825c08728dc7b0b6ba
Gabriel <gabriel.j.ortiz@gmail.com>
2018-01-01 21:25:55 -0800

    React Router, Switches, Routes including exact and render

--- ReactReduxWorkshop/dev/containers/appContainer.jsx
+++ ReactReduxWorkshop/dev/containers/appContainer.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import LoginForm from "../components/app/loginForm.jsx";
 import Home from "../components/home/home.jsx";
 import CharacterList from "../components/characters/characterList.jsx";
 
 export default class AppContainer extends Component {
-    render () {
-        return (
-            <div>
-                <LoginForm />
-                <Home />
-                <CharacterList />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <LoginForm />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/characters" exact component={CharacterList} />
+            <Route
+              render={props => (
+                <div className="spacerTop alert alert-danger">
+                  Sorry, the resource you requested ({props.match.path}) does
+                  not exist
+                </div>
+              )}
+            />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
